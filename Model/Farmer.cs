54bc6572457dd19d7cda0852c1468a1647827133
KLYA-07 <kirilllyashenko07@gmail.com>
2026-09-23 00:06:55 +0700

    Model слой готов

--- Model/Farmer.cs
+++ Model/Farmer.cs
@@ -4,7 +4,21 @@
     {
         public string farmerName { get; set; } = string.Empty;
         public int finacialCapital { get; set; }
-        public Dictionary<string, int> lastHarvest {  get; set; }
+        public float fieldSize { get; set; }
+        public Dictionary<string, int> lastHarvest { get; set; } = new Dictionary<string, int>();
+        public Dictionary<string, int> harvestCosts { get; set; } = new Dictionary<string, int>();
+        public Dictionary<string, int> cattleHeadboard { get; set; } = new Dictionary<string, int>();
+        public Dictionary<string, int> cultivatingCrops { get; set; } = new Dictionary<string, int>();
 
+        public Farmer(string farmerName, int finacialCapital, float fieldSize, Dictionary<string, int> lastHarvest, Dictionary<string, int> harvestCosts, Dictionary<string, int> cattleHeadboard, Dictionary<string, int> cultivatingCrops)
+        {
+            this.farmerName = farmerName;
+            this.finacialCapital = finacialCapital;
+            this.fieldSize = fieldSize;
+            this.lastHarvest = lastHarvest;
+            this.harvestCosts = harvestCosts;
+            this.cattleHeadboard = cattleHeadboard;
+            this.cultivatingCrops = cultivatingCrops;
+        }
     }
 }
---
+++ Model/Logic.cs
@@ -0,0 +1,75 @@
+﻿namespace Model
+{
+    public class Logic
+    {
+        private List<Farmer> allFarmers = new List<Farmer>();
+
+        public List<Farmer> AllFarmers
+        {
+            get
+            {
+                return allFarmers;
+            }
+        }
+
+        public void AddFarmer(Farmer farmer)
+        {
+            allFarmers.Add(farmer);
+        }
+
+        public void RemoveFarmer(int farmerIndex)
+        {
+            if(farmerIndex >= 0 && farmerIndex < allFarmers.Count)
+            {
+                allFarmers.RemoveAt(farmerIndex);
+            }
+        }
+
+        public Farmer ReadFarmer(int farmerIndex)
+        {
+            return allFarmers[farmerIndex];
+        }
+
+        public void ChangeFarmer(int farmerIndex, Farmer farmer)
+        {
+            allFarmers.RemoveAt(farmerIndex);
+            allFarmers.Insert(farmerIndex, farmer);
+        }
+
+        public bool ExchangeProducts(int payerFarmerIndex, int sellerFarmerIndex, string itemName, int itemsCount)
+        {
+            Farmer payer = allFarmers[payerFarmerIndex];
+            Farmer seller = allFarmers[sellerFarmerIndex];
+
+            int sum = seller.harvestCosts[itemName] * itemsCount;
+            if (payer.finacialCapital >= sum)
+            {
+                payer.finacialCapital -= sum;
+                seller.finacialCapital += sum;
+
+                payer.lastHarvest[itemName] += itemsCount;
+                seller.lastHarvest[itemName] -= itemsCount;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void HarvestSort()
+        {
+            allFarmers = allFarmers.OrderBy(h =>
+            {
+                int sum = 0;
+                foreach (var item in h.lastHarvest)
+                {
+                    sum += item.Value;
+                }
+
+                return sum;
+            }).ToList();
+        }
+    }
+}
